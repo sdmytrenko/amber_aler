157747ec6dc0ed8688b290fb4fc7ccffa8a3233b
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-11-24 15:37:40 +0200

    Add actions new, create, index for the Emergensies controller

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,11 +1,45 @@
 class EmergenciesController < ApplicationController
 
+	def new
+		@emergency = Emergency.new
+	end
+
+	def create
+		@emergency = Emergency.new(page_params)
+		if @emergency.save
+			redirect_to emergencies_path
+		else
+			render :new
+		end
+	end
+
 	def index
 		@emergencies = Emergency.all
 	end
 
+	def edit
+		@emergency = Emergency.find(:id)
+	end
 
+	def edit
+		
+	end
 
+	def update
+		
+	end
 
+	def show
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+		def page_params
+			params[:emergency].permit(:title, :description)
+		end
 
 end
