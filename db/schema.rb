17b3fefcf57a77f1009dda9fe1b036eca74823a4
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-11-26 17:54:25 +0200

    Implement Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122184757) do
+ActiveRecord::Schema.define(version: 20161126154935) do
 
   create_table "emergencies", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 20161122184757) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "emergency_id"
+    t.text     "text"
+    t.boolean  "claim_closed"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["emergency_id"], name: "index_messages_on_emergency_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
