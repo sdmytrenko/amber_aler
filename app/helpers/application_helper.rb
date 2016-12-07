73d533b91966c091eea21a38edcd0daab4d81ade
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-12-07 14:58:53 +0200

    Add WYSIWYG helper partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
+
+  def sanitize_wysiwyg(text, shorten: nil)
+    res = sanitize(text, tags: %w(p strong em u table thead tbody th tr td h4 h5 h6 pre br hr ul ol li blockquote), attributes: %w(id class style))
+    res = truncate_html(res, length: shorten, omission: '...') if shorten.present?
+    res
+  end
 end
