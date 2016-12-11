3f18fd5e121b010390b6b0297f7bde8edca60965
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-12-11 20:37:07 +0200

    Good bye, Gravatar & Identicon

--- app/assets/javascripts/identicon-avatars.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('img.avatar').each(function(i, img) {
-    var $img = $(img)
-    if ($img.prop('naturalWidth') == 0) {
-      var data = (new Identicon($img.data('hash'), 80)).toString()
-      $img.attr('src', 'data:image/png;base64,' + data)
-    }
-  })
-})
