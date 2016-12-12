254b3b9470a0e415b078df9cb2bc3db0cb5ee1df
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-12-12 17:09:30 +0200

    Add changing avatar size, create simple user profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
-  def avatar_for(user)
+  def avatar_for(user, options = { size: 80})
+    size = options[:size]
     if user.avatar?
-      image_tag user.avatar.url(:thumb)
+      image_tag user.avatar.url(:thumb), width: size
     else
-      image_tag "default_avatar.jpg"
+      image_tag "default_avatar.jpg", width: size
     end
   end
 end
