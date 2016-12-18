64b9f84a1e0f0aae9481758507d0167bcebad8f2
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-12-19 01:06:05 +0200

    Change user profile view [in progress]

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,10 +2,14 @@ module UsersHelper
   def avatar_for(user, options = { size: 80})
     size = options[:size]
     if user.avatar?
-      image_tag user.avatar.url(:thumb), width: size
+      image_tag user.avatar.url(:thumb), width: size, class: 'avatar-image'
     else
-      image_tag "default_avatar.jpg", width: size
+      image_tag "default_avatar.jpg", width: size, class: 'avatar-image'
     end
+  end
+
+    def online(user)
+      user.last_seen > 10.minutes.ago
   end
 end
 
