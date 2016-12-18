f7fd11d5ef16910f33c16283fde3af736884d7f9
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-12-18 17:55:52 +0200

    Admin panel: add admin/users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root to: 'emergencies#index'
   devise_for :users
   resources :users
+
   resources :emergencies do
       resources :messages, except: [:index, :show, :new], shallow: true
+  end
+
+  namespace :admin do
+    resources :users
   end
 end
